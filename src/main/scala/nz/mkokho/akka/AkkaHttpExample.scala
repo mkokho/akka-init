2106704e90091ccf8d58f45ff06259e4112bd37e
Mikhail Kokho <m.kokho@gmail.com>
2016-05-14 10:23:02 +1200

    Akka HTTP Example

---
+++ src/main/scala/nz/mkokho/akka/AkkaHttpExample.scala
@@ -0,0 +1,38 @@
+package nz.mkokho.akka
+
+import scala.io.StdIn
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import com.typesafe.scalalogging.StrictLogging
+
+
+object AkkaHttpExample extends StrictLogging {
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val route =
+     get {
+        pathSingleSlash {
+          logger.info(s"received request")
+          complete("Ok")
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ ⇒ system.terminate()) // and shutdown when done
+  }
+}
+
+
+
